e253013e860e0ec6697d26f2ee47930c3bef389a
Andy Simota <andyiulian.simota@gmail.com>
2024-11-05 20:52:58 +0100

    fixed even and odd

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -5,15 +5,40 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
-let userChoice = prompt("Scegli pari o dispari");
-let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+let userChoice;
+let counter = 0;
+while (userChoice !== "pari" && userChoice !== "dispari" && counter <= 10) {
+  counter++;
+  userChoice = prompt("Immetti solo pari o dispari").toLowerCase();
 }
+
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+while (userNumber !== 0 && userNumber > 5 && counter <= 10) {
+  counter++;
+  userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+}
+
+let sum = 0;
 
 let computerNumber = randomNumber(1, 5);
 
+console.log(`L'utente ha scelto ${userChoice}`);
+console.log(`L'utente ha scelto il numero ${userNumber}`);
+console.log(`Il numero del computer è ${computerNumber}`);
+
+sum = sumNumbers(userNumber, computerNumber);
+
+console.log(`La somma dei due numeri è ${sum}`);
+if (isEven(sum)) {
+  console.log(`La somma è pari`);
+} else {
+  console.log(`La somma è dispari`);
+}
+
+//function
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function sumNumbers(num1, num2) {
   return num1 + num2;
 }
@@ -21,22 +46,3 @@ function sumNumbers(num1, num2) {
 function isEven(num) {
   return num % 2 === 0;
 }
-
-console.log(`L'utente ha scelto ${userChoice}`);
-console.log(`L'utente ha scelto il numero ${userNumber}`);
-console.log(`Il numero del computer è ${computerNumber}`);
-
-console.log(
-  `La somma dei due numeri è ${sumNumbers(userNumber, computerNumber)}`
-);
-console.log;
-if (true) {
-  console.log(
-    `La somma è pari ${isEven(sumNumbers(userNumber, computerNumber))}`
-  );
-} /*  else {
-  console.log(
-    `La somma è dispari ${isEven(sumNumbers(userNumber, computerNumber))}`
-  );
-}
- */

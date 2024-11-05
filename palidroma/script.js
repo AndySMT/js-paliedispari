a44b4d891565a669c487bd705f6f5727966837f6
Andy Simota <andyiulian.simota@gmail.com>
2024-11-05 14:58:53 +0100

    Palindroma

---
+++ palidroma/script.js
@@ -0,0 +1,18 @@
+/* Palindroma
+Chiedere all’utente di inserire una Word
+Creare una funzione per capire se la Word inserita è palindroma */
+
+let word = prompt("inserisci una Parola");
+
+function palindromeWord() {
+  const char = word.split(" ");
+
+  const charInvert = char.reverse();
+
+  const invertedWord = charInvert.join(" ");
+
+  return word === invertedWord;
+}
+
+console.log(word);
+console.log(palindromeWord());
